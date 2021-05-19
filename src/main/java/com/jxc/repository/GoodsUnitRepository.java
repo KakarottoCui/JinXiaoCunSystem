230bfb3bbf0ccbaf584d514888133b0bccd85d07
KakarottoCui <471363336@qq.com>
2021-05-19 10:50:15 +0800

    first

---
+++ src/main/java/com/jxc/repository/GoodsUnitRepository.java
@@ -0,0 +1,13 @@
+package com.jxc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jxc.entity.GoodsUnit;
+
+/**
+ * 商品单位Repository接口
+ *
+ */
+public interface GoodsUnitRepository extends JpaRepository<GoodsUnit, Integer>{
+
+}
